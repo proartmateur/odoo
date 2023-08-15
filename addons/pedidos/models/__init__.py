835c0c010b94fcc0f5f253348c7b2c00b997ca1a
proartmateur <ennimandroid@gmail.com>
2023-08-15 17:33:24 -0600

    Clean

--- addons/pedidos/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import request_pedidos
-from . import product_order
-from . import sale_order
-from . import product_category
-#from . import pos_order
-#from . import models
