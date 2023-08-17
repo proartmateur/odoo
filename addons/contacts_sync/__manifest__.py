94ac4216f772ceaba7a8d38d1cb96570e4be263c
proartmateur <ennimandroid@gmail.com>
2023-08-17 15:51:33 -0600

    Creando la base del nuevo módulo Contacts Sync

--- addons/contacts_sync/__manifest__.py
+++ addons/contacts_sync/__manifest__.py
@@ -1,32 +1,31 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "contacts_sync",
+    'name': "Contacts Sync",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Sincronización de contactos entre Odoo y AgendaPro""",
 
     'description': """
-        Long description of module's purpose
+        Permite sincronizar los contactos de Odoo en AgendaPro
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Enrique Nieto Martínez",
+    'website': "https://millora.com.mx",
+    'application': True,
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Productivity',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'contacts'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/contacts_sync_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- addons/contacts_sync/models/models.py
+++ addons/contacts_sync/models/models.py
@@ -1,18 +1,67 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class contacts_sync(models.Model):
-#     _name = 'contacts_sync.contacts_sync'
-#     _description = 'contacts_sync.contacts_sync'
+class contacts_sync(models.Model):
+    _name = 'contacts.sync'
+    _description = 'contacts.sync'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    api_url = fields.Char()
+    api_username = fields.Char()
+    api_password = fields.Char()
+    sync_type = fields.Char()
+    description = fields.Text()
+
+    # region Methods
+    def sync_all_contacts(self):
+        if self.sync_type != 'massive':
+            return self.notify_danger('Ups!', 'Solamente se pueden sincronizar todos los contactos cuando el tipo de actualización es atómico. Presione click sobre el botón: "HABILITAR MASIVO" y vuelva a intentarlo nuevamente.')
+        print("------> Sincronizando..........")
+        return self.notify_success("Listo","¡Todos los contactos fueron sincronizados!")
+
+    #region Notifications
+    def show_notification(self, title:str, message:str, type:str):
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': title,
+                'message': message,
+                'type': type, #types: success,warning,danger,info
+                'sticky': False,
+            }
+        }
+
+    def notify_danger(self, title, message):
+        return self.show_notification(title, message, 'danger')
+
+    def notify_success(self, title, message):
+        return self.show_notification(title, message, 'success')
+
+    def notify_warning(self, title, message):
+        return self.show_notification(title, message, 'warning')
+
+    #endregion
+    def set_massive_available(self):
+        self.sync_type = 'massive'
+        self.description = f"""Sincronización Masiva
+        Este tipo de sincronización bloquea los eventos: (Cuando se crea un contacto) y (Cuando se actualiza un contacto)
+        con la finalidad de prevenir un ciclo infinito de actualizaciones entre Odoo y {self.name}.
+        
+        Al presionar click en el botón "SINCRONIZAR TODOS" se comenzará a sicronizar todos los contactos de Odoo con {self.api_url}
+        dando como resultado final la misma información en ambos sistemas.
+        Se dará prioridad a la información de Odoo, por lo que si algún contacto existe previamente en {self.name}
+        su información será sobreescrita con la versión de Odoo.
+        En caso de que el contacto solamente exista en {self.name} este se creará en Odoo con la misma información.
+        """
+
+    def set_atomic_available(self):
+        self.sync_type = 'atomic'
+        self.description = f"""Sincronización Atómica
+                Este tipo de sincronización detecta los eventos en Odoo: (Cuando se crea un contacto) y (Cuando se actualiza un contacto)
+                Al momento de ejecturase, va a sobreescribir la información del contacto existente en {self.name}.
+                En caso de no existir en {self.name} se creará un nuevo contacto con la información de Odoo.
+                """
+    # endregion
